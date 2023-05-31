20177137b9aa680815a0997ad367181f8fd9ebfb
iaswmn <evg.ykvts@gmail.com>
2023-05-31 10:57:44 +0300

    fix: refactor code

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, StringProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 from kivy.core.audio import SoundLoader
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
 
 log_config = LogConfig()
 logger = log_config.configurate_log()
@@ -38,11 +38,11 @@ class PongPaddle(Widget):
 
     def __init__(self, **kwargs):
         super(PongPaddle, self).__init__(**kwargs)
-        self.otskok_sound = SoundLoader.load(PathEnum.BOUNCE_SOUND_PATH_ENUM)
+        self.bounce_sound = SoundLoader.load(PathEnum.BOUNCE_SOUND_PATH_ENUM)
 
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
-            self.otskok_sound.play()
+            self.bounce_sound.play()
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
             bounced = Vector(-1 * vx, vy)
@@ -52,7 +52,6 @@ class PongPaddle(Widget):
 
 
 class PongGame(Widget):
-    backgrounds_list = os.listdir(PathEnum.BACKGROUNDS_PATH_ENUM)
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
@@ -61,6 +60,7 @@ class PongGame(Widget):
     result = StringProperty('')
     start_time = NumericProperty(0)
     elapsed_time = NumericProperty(0)
+    backgrounds_list = os.listdir(PathEnum.BACKGROUNDS_PATH_ENUM)
     background_image = StringProperty(PathEnum.BACKGROUNDS_PATH_ENUM + '/' + backgrounds_list[0])
 
     def __init__(self, **kwargs):
@@ -70,12 +70,6 @@ class PongGame(Widget):
         self.background_image_button = None
         self.ball_button = None
         self.white_sound = SoundLoader.load(PathEnum.WHITE_SOUND_PATH_ENUM)
-        self.end_button = Button(text=ButtonNamesEnum.RETURN_TO_MENU_BTN_ENUM, size_hint=(None, None), size=(350, 150),
-                                 pos_hint={'x': 0.7})
-        self.end_button.bind(on_press=self.show_results)  # Изменяем привязку кнопки к новому методу
-        self.end_button.disabled = True
-        self.end_button.opacity = 0
-
         self.move_paddle_event = None  # Event for moving paddles
 
     def change_background(self, instance):
@@ -130,20 +124,13 @@ class PongGame(Widget):
         if self.vs_ai:
             self.move_ai_paddle()
 
-        if self.player1.score == 5:
+        self.check_results()
+
+    def check_results(self):
+        if self.player1.score == 5 or self.player2.score == 5:
             self.is_running = False
-            self.end_button.disabled = False
-            self.end_button.opacity = 1
             Clock.unschedule(self.update)
-            logger.debug('Game: Red Win')
-            self.show_results()  # Вызываем метод для вывода результатов
-        elif self.player2.score == 5:
-            logger.debug('Game: Blue Win')
-            self.is_running = False
-            self.end_button.disabled = False
-            self.end_button.opacity = 1
-            Clock.unschedule(self.update)
-            self.show_results()  # Вызываем метод для вывода результатов
+            self.show_results()
 
     def on_touch_move(self, touch):
         if self.vs_ai:
@@ -162,13 +149,12 @@ class PongGame(Widget):
         else:
             winner = "Blue Wins!"
 
+        logger.debug(f'Game: {winner}')
+
         score_message = "Red Score: {}\nBlue Score: {}\nElapsed Time: {:.2f} seconds".format(
             self.player1.score, self.player2.score, self.elapsed_time)
 
-        # Создаем виджет Label для победителя
         winner_label = Label(text=winner, font_size='40sp')
-
-        # Создаем виджет Label с остальными результатами
         score_label = Label(text=score_message)
 
         # Создаем BoxLayout для размещения виджетов Label
@@ -180,20 +166,12 @@ class PongGame(Widget):
         message_box = Popup(title="Game Over", content=layout, size_hint=(None, None), size=(700, 700))
         message_box.open()
 
-        self.return_to_main_menu()  # Вызываем метод возврата в главное меню
+        self.return_to_main_menu()
 
     def return_to_main_menu(self):
         self.is_running = False
+        self.enable_menu()
         self.reset_score()
-        self.remove_widget(self.end_button)
-        self.start_button.disabled = False
-        self.start_button.opacity = 1
-        self.mode_button.disabled = False
-        self.mode_button.opacity = 1
-        self.background_image_button.disabled = False
-        self.background_image_button.opacity = 1
-        self.ball_button.disabled = False
-        self.ball_button.opacity = 1
         Clock.unschedule(self.update)
         self.ball.velocity = (0, 0)
         self.ball.center = self.center
@@ -202,21 +180,10 @@ class PongGame(Widget):
         self.elapsed_time = 0
         logger.debug('Game: Returned to main menu')
 
-
     def start_game(self, instance):
         self.is_running = True
-        instance.disabled = True
-        instance.opacity = 0
-        self.mode_button.disabled = True
-        self.mode_button.opacity = 0
-        self.background_image_button.disabled = True
-        self.background_image_button.opacity = 0
-        self.ball_button.disabled = True
-        self.ball_button.opacity = 0
-        self.add_widget(self.end_button)
+        self.disable_menu()
         self.reset_score()
-        self.end_button.disabled = True
-        self.end_button.opacity = 0
         self.serve_ball()
         self.start_time = datetime.now().timestamp()
         logger.debug('Game: Match started')
@@ -248,6 +215,26 @@ class PongGame(Widget):
             self.player2.center_y += min(4.5, self.ball.center_y - self.player2.center_y)
         elif self.ball.center_y < self.player2.center_y:
             self.player2.center_y -= min(4.5, self.player2.center_y - self.ball.center_y)
+
+    def disable_menu(self):
+        self.start_button.disabled = True
+        self.start_button.opacity = 0
+        self.mode_button.disabled = True
+        self.mode_button.opacity = 0
+        self.background_image_button.disabled = True
+        self.background_image_button.opacity = 0
+        self.ball_button.disabled = True
+        self.ball_button.opacity = 0
+
+    def enable_menu(self):
+        self.start_button.disabled = False
+        self.start_button.opacity = 1
+        self.mode_button.disabled = False
+        self.mode_button.opacity = 1
+        self.background_image_button.disabled = False
+        self.background_image_button.opacity = 1
+        self.ball_button.disabled = False
+        self.ball_button.opacity = 1
 
 
 class PongApp(App):
